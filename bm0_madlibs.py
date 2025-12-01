faab1736f11ccf22012ef62711ea99df44e1d251
Eth-res <ethanr155@nycstudents.net>
2025-12-01 14:48:09 -0500

    Update bm0_madlibs.py

--- bm0_madlibs.py
+++ bm0_madlibs.py
@@ -1,1 +1,3 @@
-print('Hello World')
+verb = input("Please enter a verb (action word):")
+noun = input("Please enter a noun (person, place, or thing):")
+adjective = input("Please enter a adjective (describes):")
