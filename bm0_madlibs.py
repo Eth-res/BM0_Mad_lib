7ab1b497391f2442b080684f0711718a6759164a
Eth-res <ethanr155@nycstudents.net>
2025-12-04 11:29:13 -0500

    Update bm0_madlibs.py
    finished

--- bm0_madlibs.py
+++ bm0_madlibs.py
@@ -1,3 +1,7 @@
 verb = input("Please enter a verb (action word):")
 noun = input("Please enter a noun (person, place, or thing):")
 adjective = input("Please enter a adjective (describes):")
+
+print(f"I decided to {verb} ")
+print(f"I {verb} all the way to {noun}")
+print(f"when i got to {noun} it was very {adjective} ")
